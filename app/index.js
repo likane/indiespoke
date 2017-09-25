be2d076ff508adb32bd6299538c85c802bd5a303
louiskane <lkane02@yahoo.com>
2017-09-24 23:44:27 -0700

    added hover effects

--- app/index.js
+++ app/index.js
@@ -1,9 +1,15 @@
 
-
-// $(window).load(function(){});
+$(document).ready(function(){
 
 // $(".card-img-overlay").hide();
 
-// $('.card').hover(function() {
-// 	$('.card-img-overlay').show('slow');
-// });
+// $(".card").hover(function() {
+// 	$(".card-img-overlay").show("slow");
+// 	$(".card").css("rgba(0,0,0, 0.5)");
+	
+// 	});
+
+
+});
+//end of document.ready
+
